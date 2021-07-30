b3855750d85621f2367d61a28355752d2ef0a170
unknown <lol648418@gmail.com>
2021-07-30 16:16:18 +0400

    buttonSection

--- src/components/rowItem.vue
+++ src/components/rowItem.vue
@@ -1,32 +1,27 @@
 <template>
-  <div :id="`button-section-` + this.$props.id" class="button-section d-flex mt-2 justify-content-between">
-    <h4 class="bg-primary text-white text-center fw-bold">{{ count }}</h4>
-    <button @click="plus($event)" class="bg-secondary btn text-white fw-bold"><i class="fas fa-plus-circle"></i></button>
-    <button @click="minus($event)" class="bg-info btn text-white fw-bold"><i class="fas fa-minus-circle"></i></button>
-    <button @click="deleteRow($event)" class="bg-danger btn text-white fw-bold"><i class="fas fa-trash"></i></button>
+  <div class="button-section d-flex mt-2 justify-content-between">
+    <h4 class="bg-primary text-white text-center fw-bold">{{ value }}</h4>
+    <button @click="plus()" class="bg-secondary btn text-white fw-bold"><i class="fas fa-plus-circle"></i></button>
+    <button @click="minus()" class="bg-info btn text-white fw-bold"><i class="fas fa-minus-circle"></i></button>
+    <button @click="deleteFunction()" class="bg-danger btn text-white fw-bold"><i class="fas fa-trash"></i></button>
   </div>
 </template>
 <script>
 export default {
-  props: ["value","id"],
-  data(){
-    return{
-      count: parseInt(this.$props.value)
+  props: ["value","index"],
+
+  methods:{
+    plus(){
+      this.$emit("plus",this.$props.index);
+    },
+    minus(){
+      if(this.$props.value > 0) {
+        this.$emit("minus",this.$props.index);
+      }
+    },
+    deleteFunction() {
+      this.$emit("delete",this.$props.index);
     }
   },
-  methods:{
-  plus(event){
-    this.count += 1;
-  },
-  minus(event){
-    if(this.count > 0){
-      this.count -= 1;
-  }
-},
-  deleteRow(event) {
-    event.currentTarget.parentElement.remove()
-  }
-}
-
 }
 </script>
--- src/components/task.vue
+++ src/components/task.vue
@@ -9,29 +9,45 @@
         <button class="btn-primary btn ms-2" @click="restore">Restore <img src="../assets/restore.svg" width="20px" alt=""></button>
     </div>
     <div class="rows">
-      <rowItem v-for="i in 5" :key="i" value="0" :id="i"></rowItem>
+      <rowItem v-for="(row,index) in rowArray" :key="index" :index="index" :value="row" @plus="plus(index)" @minus="minus(index)" @delete="deleteRow($event)"></rowItem>
     </div>
   </div>
 </template>
 <script>
 import rowItem from "./rowItem"
 export default {
-  data(){
-    return{
-      rows: null
-    }
-  },
   components: {
     "rowItem": rowItem,
   },
+  data(){
+    return{
+      rowArray: [0, 1, 3, 0, 1],
+      divRow: null,
+      removedArray: null,
+    }
+  },
   methods: {
     restore(){
-     this.rows = document.querySelector('.rows')
-      console.log(this.rows)
-      console.log(this.rows.children)
-      if(this.rows.children.length == 0){
-        console.log("empty")
+    },
+    plus(index){
+      console.log("asda")
+      this.rowArray = this.rowArray.splice(0,this.rowArray.length)
+    },
+    minus(index){
+      console.log("asda")
+      this.changedRow = this.rowArray
+      if( this.changedRow[index] > 0){
+        this.changedRow[index] -= 1
       }
+      this.rowArray = this.changedRow
+    },
+    deleteRow(index){
+      console.log("The index is  " + index)
+      const rowArrayClone = [...this.rowArray];
+      rowArrayClone.splice(index, 1);
+      console.log('rowArrayClone', rowArrayClone);
+      this.rowArray = rowArrayClone;
+      console.log(this.rowArray)
     }
   },
 
